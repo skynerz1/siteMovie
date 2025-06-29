5569ef26be2a3a5431e048337a1eebb64f077a83
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-06-29 07:08:51 +0300

    Update series.php

--- series.php
+++ series.php
@@ -445,6 +445,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+function toggleNav() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
+
+
 </script>
 
 </body>
