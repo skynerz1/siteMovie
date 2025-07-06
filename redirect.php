6d57350adc0ee40856875b91af5dae51eb815d25
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-07-06 16:06:52 +0300

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,6 +1,21 @@
 <?php
-// المسار الكامل داخل موقعك
-$redirectUrl = './?page=2&search=' . urlencode('لعبه الحبار');
+// خريطة إعادة التوجيه حسب قيمة المعامل s
+$redirectMap = [
+    'squad' => './?page=2&search=' . urlencode('لعبه الحبار'),
+    'ommi' => './series.php?id=24072',
+    // تقدر تضيف هنا قيم اخرى
+];
+
+// قراءة قيمة المعامل 's' من الرابط
+$s = $_GET['s'] ?? '';
+
+// إذا القيمة موجودة بالخريطة، نعيد التوجيه عليها
+if (array_key_exists($s, $redirectMap)) {
+    $redirectUrl = $redirectMap[$s];
+} else {
+    // لو القيمة غير موجودة نعيد التوجيه لصفحة افتراضية
+    $redirectUrl = './'; // أو اي رابط تحب
+}
 
 // إعادة التوجيه
 header("Location: $redirectUrl");
