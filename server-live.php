a5ca929f3d6505f9bee104a8f576b01dac2ee749
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-07-16 13:54:00 +0300

    Update server-live.php

--- server-live.php
+++ server-live.php
@@ -563,7 +563,8 @@ function submitReport(event) {
 
 
             <!-- نموذج البلاغ -->
-                <div id="reportModal" style="display:none; position:fixed; inset:0; background:rgba(0,0,0,0.7); z-index:9999; align-items:center; justify-content:center;">
+<div id="reportModal" style="display:none; position:fixed; inset:0; background:rgba(0,0,0,0.7); z-index:9999; display: flex; align-items:center; justify-content:center;">
+
 
               <div style="background:#1a1a1a; padding:30px; border-radius:10px; width:90%; max-width:400px; color:#fff;">
                 <h3 style="color:#e50914;">إرسال بلاغ</h3>
