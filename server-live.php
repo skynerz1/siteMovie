3972edf3495e997f6f681f3ef3134ab435584480
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-07-16 11:14:02 +0300

    Update and rename a.php to server-live.php

--- server-live.php
+++ server-live.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
-  
+
 <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>DFkz Player مع اختيار القنوات والإعدادات</title>
@@ -521,9 +521,13 @@
   // بيانات القنوات (مثال مع شعار ورابط بث m3u8)
   const channels = [
     { id: 1, name: "mbc1", url: "https://d3o3cim6uzorb4.cloudfront.net/out/v1/0965e4d7deae49179172426cbfb3bc5e/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/a7dcf0c9-1178-4cb9-a490-a8313975e37c?height=129&width=230&croppingPoint=&version=1&type=avif" },
-    
+
+      { id: 18, name: "ssc-1", url: "https://stream.sainaertebat.com/hls2/ssc1.m3u8", logo: "https://shahid.mbc.net/mediaObject/8abc6233-1ef2-443b-8de6-d401a60aa025?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+    { id: 19, name: "bein-1", url: "https://wo.cma.footballii.ir/hls2/b1_src.m3u8", logo: "https://upload.wikimedia.org/wikipedia/commons/8/85/Logo_beIN.png" },
+
     { id: 2, name: "mbc2", url: "https://edge66.magictvbox.com/liveApple/MBC_2/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/0fc148ad-de25-4bf6-8fc8-5f8f97a52e2d?height=129&width=230&croppingPoint=&version=1&type=avif" },
-    
+
     { id: 3, name: "mbc3", url: "https://mbcbollywood-prod-dub-ak.akamaized.net/out/v1/d5bbe570e1514d3d9a142657d33d85e6/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/05162db8-9f01-4aeb-95e8-52aba8baf609" },
 
     { id: 4, name: "mbc4", url: "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-4/24f134f1cd63db9346439e96b86ca6ed/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/e4658f69-3cac-4522-a6db-ff399c4f48f1?height=129&width=230&croppingPoint=&version=1&type=avif" },
@@ -531,10 +535,31 @@
     { id: 5, name: "mbc5", url: "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-5/ee6b000cee0629411b666ab26cb13e9b/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/94786999-8a35-4e25-abc6-93680bd3b457?height=129&width=230&croppingPoint=&version=1&type=avif" },
 
     { id: 6, name: "mbc6", url:"https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-bollywood/546eb40d7dcf9a209255dd2496903764/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/ce2f5296-90ea-48f2-a997-125df5d73b42?height=129&width=230&croppingPoint=&version=1&type=avif" },
-    
+
+      { id: 7, name: "mbc-masr", url:"https://shls-masr-prod-dub.shahid.net/out/v1/d5036cabf11e45bf9d0db410ca135c18/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/2c600ff4-bd00-4b99-b94d-b178a7366247?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+        { id: 17, name: "روتانا خليجيه", url:"https://edge66.magictvbox.com/liveApple/rotana_khalijiah/tracks-v1a1/mono.m3u8", logo: "https://www.okaz.com.sa/uploads/images/2020/06/01/1574565.JPG" },
+
+        { id: 16, name: "وناسه", url:"https://shls-wanasah-prod-dub.shahid.net/out/v1/c84ef3128e564b74a6a796e8b6287de6/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/97613919-40eb-4032-9dcb-e940e08ae761?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+        { id: 13, name: "رؤيا", url:"https://royatv-live.daioncdn.net/royatv/royatv.m3u8", logo: "https://jordandir.com/images/screenshots/1711030162.webp" },
+
+        { id: 8, name: "قطر-1", url:"https://live.kwikmotion.com/qtv1live/qtv1.smil/playlist.m3u8", logo: "https://yt3.googleusercontent.com/pcLGQIWlrO000zyC8SEZzOmm3iZmDAmMQSNRTG28toSt9p-QX88NuiEc4GCmfXk8EwH3twcb=s900-c-k-c0x00ffffff-no-rj" },
+
+      { id: 14, name: "سما دبي", url:"https://dmieigthvllta.cdn.mgmlcdn.com/samadubaiht/smil:samadubai.stream.smil/chunklist_b8500000.m3u8", logo: "https://admango.cdn.mangomolo.com/analytics/uploads/71/5fb0fc1d19.png" },
+
+        { id: 15, name: "دبي", url:"https://dmieigthvllta.cdn.mgmlcdn.com/dubaitvht/smil:dubaitv.stream.smil/chunklist.m3u8", logo: "https://admango.cdn.mangomolo.com/analytics/uploads/71/659cd942e4.png" },
+
+      { id: 9, name: "الجزيره", url:"https://live-hls-apps-aja-fa.getaj.net/AJA/index.m3u8", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9hF9gGUDglabva3DrXaW7yGedHdx0nQoFztnuMXjBeNjCbEch9JM-omyLGH5xyLPeuRI&usqp=CAU" },
+
+        { id: 10, name: "العربي", url:"https://alarabyta.cdn.octivid.com/alaraby/smil:alaraby.stream.smil/chunklist.m3u8", logo: "https://yt3.googleusercontent.com/dirOUBiyFLsQqf58hs78w2NRbQu2u3SfXr77jlH6y1mDwh4TpEtI5CXzhpCy8Aw7tz6CgveWbw=s900-c-k-c0x00ffffff-no-rj" },
+
+      { id: 11, name: "الاخباريه", url:"https://cdn-globecast.akamaized.net/live/eds/al_ekhbariya/hls_roku/index.m3u8", logo: "https://upload.wikimedia.org/wikipedia/commons/e/e3/%D8%A7%D9%84%D9%82%D9%86%D8%A7.png" },
+
+        { id: 12, name: "الحدث", url:"https://av.alarabiya.net/alarabiapublish/alhadath.smil/playlist.m3u8", logo: "https://yt3.googleusercontent.com/ehhpuQeVHO0g3kIPkmwrw1x0fLqDk7RyWH733oe4wcKb_1jBEMvGt4WVlQEEzcTCL6zq01K5HQ=s900-c-k-c0x00ffffff-no-rj" },
   ];
 
-  
+
   function getChannelFromURL() {
     const params = new URLSearchParams(window.location.search);
     const chId = parseInt(params.get("ch"));
