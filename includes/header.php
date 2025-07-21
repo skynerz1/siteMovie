31c34519a7062e32efd0192159ed065ab478a19e
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-07-21 22:19:13 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -320,7 +320,7 @@
         <li><a href="../index.php"><i class="fas fa-home"></i> Home</a></li>
         <li><a href="../movie"><i class="fas fa-film"></i> Movies</a></li>
         <li><a href="../index.php"><i class="fas fa-tv"></i> Series</a></li>
-        <li><a href="/server-live.php#channel"><i class="fas fa-broadcast-tower"></i> Channels</a></li>
+        <li><a href="/cat.php?category=channels"><i class="fas fa-broadcast-tower"></i> Channels</a></li>
         <li><a href="../favorites.php"><i class="fas fa-heart"></i> Favorites</a></li>
         <li><a href="../contact.php"><i class="fas fa-envelope"></i> Contact</a></li>
       </ul>
@@ -347,7 +347,7 @@
       <i class="fas fa-film"></i>
       <span>أفلام</span>
     </a>
-    <a href="/server-live.php#channel" class="nav-item">
+    <a href="/cat.php?category=channels" class="nav-item">
       <i class="fas fa-broadcast-tower"></i>
       <span>لايف</span>
     </a>
