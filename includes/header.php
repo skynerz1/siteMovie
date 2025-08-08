0a5200cf192b98e250e654ba7539bdb7835c10b0
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-08-08 05:27:31 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,8 @@
   <title>DFKZ WATCH MOVIE & SERIES & CHANNEL</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
   <link rel="stylesheet" href="styles.css">
+<!-- ====== Hero Slider ====== -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
  <style>
     * {
       box-sizing: border-box;
