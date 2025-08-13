ec8b43a6fdca630221a0d23c788be21f5e5b4ccb
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-08-13 14:19:55 +0300

    Update api-live.php

--- api-live.php
+++ api-live.php
@@ -522,12 +522,6 @@
   const channels = [
     { id: 1, name: "mbc1", url: "https://d3o3cim6uzorb4.cloudfront.net/out/v1/0965e4d7deae49179172426cbfb3bc5e/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/a7dcf0c9-1178-4cb9-a490-a8313975e37c?height=129&width=230&croppingPoint=&version=1&type=avif" },
 
-      { id: 18, name: "ssc-1", url: "https://proxy.anime.gt.tc/m3u8-proxy?url=https://stream.sainaertebat.com/hls2/ssc1.m3u8", logo: "https://shahid.mbc.net/mediaObject/8abc6233-1ef2-443b-8de6-d401a60aa025?height=129&width=230&croppingPoint=&version=1&type=avif" },
-
-    { id: 19, name: "bein-1", url: "https://proxy.anime.gt.tc/m3u8-proxy?url=https://wo.cma.footballii.ir/hls2/b1_src.m3u8", logo: "https://upload.wikimedia.org/wikipedia/commons/8/85/Logo_beIN.png" },
-
-    { id: 23, name: "ريال مدريد", url: "https://proxy.anime.gt.tc/m3u8-proxy?url=https://rmtv.akamaized.net/hls/live/2043154/rmtv-en-web/bitrate_3.m3u8", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStqDmOsCegryFEhcARKy82qGAK_q1_HRFH4IwoLUHp8c_Fi3B55nW3FdFZxa5X4xjyeSo&usqp=CAU" },
-
     { id: 2, name: "mbc2", url: "https://edge66.magictvbox.com/liveApple/MBC_2/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/0fc148ad-de25-4bf6-8fc8-5f8f97a52e2d?height=129&width=230&croppingPoint=&version=1&type=avif" },
 
     { id: 3, name: "mbc3", url: "https://mbcbollywood-prod-dub-ak.akamaized.net/out/v1/d5bbe570e1514d3d9a142657d33d85e6/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/05162db8-9f01-4aeb-95e8-52aba8baf609" },
@@ -541,6 +535,32 @@
     { id: 24, name: "MBC DRAMA", url:"https://shls-live-ak.akamaized.net/out/v1/b0b3a0e6750d4408bb86d703d5feffd1/index_19.m3u8", logo: "https://shahid.mbc.net/mediaObject/4bac4257-39fa-4e00-b91e-befdcff0091a?height=129&width=230&croppingPoint=&version=1&type=avif" },
 
       { id: 7, name: "mbc-masr", url:"https://shls-masr-prod-dub.shahid.net/out/v1/d5036cabf11e45bf9d0db410ca135c18/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/2c600ff4-bd00-4b99-b94d-b178a7366247?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+
+        { id: 25, name: "mbc-masr2", url:"https://m3u.up.railway.app/m3u8-proxy?url=https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-masr-2/754931856515075b0aabf0e583495c68/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/997486ba-a0e4-4d75-8575-6dd2e341adee?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+           { id: 26, name: "mbc-masr Drama", url:"https://m3u.up.railway.app/m3u8-proxy?url=https://shls-live-enc.edgenextcdn.net/out/v1/08eca926a78a41339b8010c882410307/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/8c152402-a127-4bcf-89a8-4aaf6ac000ff?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+
+
+
+ { id: 27, name: "mbc Gaming", url:"https://m3u.up.railway.app/m3u8-proxy?url=https://shls-live-enc.edgenextcdn.net/out/v1/8ae5c35ffc5247a58e71a5c86dd31ad6/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/dee97619-f135-409c-88a4-470a41018a5e?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+  { id: 28, name: "أفلام", url:"https://m3u.up.railway.app/m3u8-proxy?url=https://shls-live-enc.edgenextcdn.net/out/v1/0044dd4b001a466c941ad77b04a574a2/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/e63c71af-6e93-43f6-8713-21ab41b46ead?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+    { id: 29, name: "باب الحاره", url:"https://m3u.up.railway.app/m3u8-proxy?url=https://shls-live-enc.edgenextcdn.net/out/v1/948c54279b594944adde578c95f1d7d1/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/2d38f35a-a36c-4fb7-ac80-d639bda10cd5?height=129&width=230&croppingPoint=&version=1&type=avif" },    
+
+  { id: 30, name: "مرايا", url:"https://m3u.up.railway.app/m3u8-proxy?url=https://shls-live-enc.edgenextcdn.net/out/v1/a4a39d8e92e34b0780ca602270a59512/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/60a952c2-cfe6-4c21-bc51-11108e17fef3?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+  { id: 31, name: "افلام اكشن", url:"https://m3u.up.railway.app/m3u8-proxy?url=https://shls-live-enc.edgenextcdn.net/out/v1/46079e838e65490c8299f902a7731168/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/d1a30edd-509d-49dd-8706-4adac4c51ffc?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+  { id: 32, name: "افلام اثاره", url:"https://m3u.up.railway.app/m3u8-proxy?url=https://shls-live-enc.edgenextcdn.net/out/v1/f6d718e841f8442f8374de47f18c93a7/index.m3u8", logo: "https://shahid.mbc.net/mediaObject/9c15e63b-2780-4617-92c7-24ac142920a2?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+      { id: 18, name: "ssc-1", url: "https://m3u.up.railway.app/m3u8-proxy?url=https://stream.sainaertebat.com/hls2/ssc1.m3u8", logo: "https://shahid.mbc.net/mediaObject/8abc6233-1ef2-443b-8de6-d401a60aa025?height=129&width=230&croppingPoint=&version=1&type=avif" },
+
+    { id: 19, name: "bein-1", url: "https://m3u.up.railway.app/m3u8-proxy?url=https://wo.cma.footballii.ir/hls2/b1_src.m3u8", logo: "https://upload.wikimedia.org/wikipedia/commons/8/85/Logo_beIN.png" },
+
+    { id: 23, name: "ريال مدريد", url: "https://m3u.up.railway.app/m3u8-proxy?url=https://rmtv.akamaized.net/hls/live/2043154/rmtv-en-web/bitrate_3.m3u8", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStqDmOsCegryFEhcARKy82qGAK_q1_HRFH4IwoLUHp8c_Fi3B55nW3FdFZxa5X4xjyeSo&usqp=CAU" },
 
       { id: 22, name: "سبيستون", url:"https://streams.spacetoon.com/live/stchannel/smil:livesmil.smil/chunklist_w819131178_b1296000_slAR.m3u8", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTsuYYAbLWXDmf678ip7wKmr1zvj3PlhOnig&s" },
 
